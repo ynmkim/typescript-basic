818b962ad837c187b84e5fc6d7f1cebb63c18390
김윤미 <younmeet@gmail.com>
2023-09-11 15:44:49 +0900

    모듈, 빌드 설정

---
+++ dist/main.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const product1 = {
+    id: 'c001',
+    name: '코드잇 블랙 후드 집업',
+    price: 129000,
+    membersOnly: true,
+    sizes: ['Size.S', 'Size.M'],
+};
+const product2 = {
+    id: 'd001',
+    name: '코드잇 텀블러',
+    price: 25000,
+};
+console.log(product1);
+console.log(product2);
